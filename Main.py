666b7c91be3eb2eb1cdcc7b1989d312d98617b92
Ines C <63038099+Nessouille@users.noreply.github.com>
2025-02-06 17:56:38 +0000

    Dealing with missing main.py file

--- Main.py
+++ Main.py
@@ -1,1 +1,12 @@
+import DataCleaning as clean
+import DataFetching as fetch
+from LoggerConfig import logger
+import Model
 
+def main():
+    logger.info('Script started')
+
+#CLASS DEFIINITION 
+if __name__ == '__main__':
+    main()
+    logger.info("Running as main")
